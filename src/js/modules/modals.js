a49116e0e0d1c98a11d9e9c001232656662d0afb
Kostantin Chirov <93807284+KonstaninC@users.noreply.github.com>
2022-04-08 09:58:22 +0800

    done project

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -60,7 +60,7 @@ const modals = () => {
   }
 
   function calcScroll(){
-    let div= document.createElement('div')
+    let div= document.createElement('div');
 
     div.style.width= '50px';
     div.style.height= '50px';
@@ -70,7 +70,7 @@ const modals = () => {
     document.body.appendChild(div);
 
     let scrollWidth= div.offsetWidth- div.clientWidth;
-    div.remove;
+    div.remove();
 
     return scrollWidth;
   }
