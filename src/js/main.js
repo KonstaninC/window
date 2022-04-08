9db7ab2561c45f750c4f45a58c587bb3d0347ea3
Kostantin Chirov <93807284+KonstaninC@users.noreply.github.com>
2022-04-08 08:28:17 +0800

    add timer

--- src/js/main.js
+++ src/js/main.js
@@ -6,12 +6,14 @@ import tabs from './modules/tabs';
 import forms from './modules/forms';
 import changeModalState from './modules/changeModalState';
 import checkNumInputs from './modules/checkNumInputs';
+import timer from './modules/timer';
 
 
 window.addEventListener('DOMContentLoaded', () => {
   'use strict';
 
   let modalState = {};
+  let deadline = '2022-05-25';
 
   changeModalState(modalState);
   modals();
@@ -19,4 +21,5 @@ window.addEventListener('DOMContentLoaded', () => {
   tabs('.decoration_slider', '.no_click', '.decoration_content >div >div', 'after_click');
   tabs('.balcon_icons', '.balcon_icons_img', '.big_img > img', 'do_image_more', 'inline-block');
   forms(modalState);
+  timer('.container1', deadline);
 });
